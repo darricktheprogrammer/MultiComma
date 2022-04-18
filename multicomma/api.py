f70b4dc76d7ed446e896099fb4eb10c171859617
Darrick Herwehe <darrick@exitcodeone.com>
2022-04-17 21:44:33 -0400

    fix line endings not splitting correctly
    Sublime does not return the escape code (\n, \r\n),
    but only the description (unix, windows). The value
    must be converted for splitting joining strings.

--- multicomma/api.py
+++ multicomma/api.py
@@ -5,10 +5,23 @@ A longer description...
 """
 
 import sublime_plugin
+from .manipulations import add_comma_to_line_multi
 
 
 class MultiCommaCommand(sublime_plugin.TextCommand):
 	characters_to_prepend = " ]}),"
 
+	LINE_ENDINGS = {
+		"unix": "\n",
+		"windows": "\r\n",
+	}
+
+	def _line_endings(self):
+		return self.LINE_ENDINGS[self.view.line_endings().lower()]
+
 	def run(self, edit, **args):
 		for region in self.view.sel():
+			expanded_region = self.view.line(region)
+			txt = self.view.substr(expanded_region)
+			with_comma = add_comma_to_line_multi(txt, self._line_endings())
+			self.view.replace(edit, expanded_region, with_comma)
