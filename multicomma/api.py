5da4b359248de5ae23f91ed42562e5775a1e27b8
Darrick Herwehe <darrick@exitcodeone.com>
2022-05-17 12:04:52 -0400

    WIP

--- multicomma/api.py
+++ multicomma/api.py
@@ -27,8 +27,16 @@ class MultiCommaCommand(sublime_plugin.TextCommand):
 			txt = self.view.substr(full_line_region)
 			if txt.endswith(","):
 				continue
+
 			is_reverse_selection = region.a > region.b
 			begin = region.a if not is_reverse_selection else region.b
+
+			if region.empty():
+				test_region = Region(region.a - 1, region.b)
+			else:
+				end_point = max(region.a, region.b)
+				test_region = Region(end_point - 1, end_point)
+
 
 			with_comma = add_comma_to_line_multi(txt, self._line_endings())
 			self.view.replace(edit, full_line_region, with_comma)
@@ -42,15 +50,30 @@ class MultiCommaCommand(sublime_plugin.TextCommand):
 				else edited_region.a
 			)
 
+
+
 			self.view.sel().subtract(self.view.sel()[i])
 			self.view.sel().subtract(full_line_region)
 			self.view.sel().subtract(region)
 
 			if region.empty():
+				if self.view.substr(test_region) in self.characters_to_prepend and region.b == full_line_region.b:
+					region.a += 1
+					region.b += 1
+
 				self.view.sel().add(region)
 			elif is_reverse_selection:
+				print()
+				if self.view.substr(test_region) in self.characters_to_prepend and end == full_line_region.a:
+					begin += 1
+					end += 1
+
 				new_region = Region(end - 1, begin)
 				self.view.sel().add(new_region)
 			else:
+				if self.view.substr(test_region) in self.characters_to_prepend and region.b == full_line_region.b:
+					begin += 1
+					end += 1
+
 				new_region = Region(begin, end - 1)
 				self.view.sel().add(new_region)
